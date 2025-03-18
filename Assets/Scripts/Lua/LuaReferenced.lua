1591b2c1b26d51eb8d6e3e9a76a3a417ae66429e
FarahXR <ATBB9vY5WVvxX6es8uDTgbTx4AVc4D6DC0A2>
2025-03-18 22:52:12 +0100

    New changes

--- Assets/Scripts/Lua/LuaReferenced.lua
+++ Assets/Scripts/Lua/LuaReferenced.lua
@@ -1,4 +1,13 @@
+local unity = CS.UnityEngine
+local cs_coroutine = (require 'CSCoroutine')
+
 function do_something()
 	CS.UnityEngine.Debug.LogWarning("do_something was called")
 	--do_something()
+
+	cs_coroutine.start(self, function() -- can pass any function
+		coroutine.yield(CS.UnityEngine.WaitForSeconds(3))
+		unity.GameObject.Destroy(gameObject)
+		CS.UnityEngine.Debug.LogWarning("GameObject deleted!")
+	end)
 end
